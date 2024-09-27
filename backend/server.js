5a0d8de06abb7a957eb2c45fd5b68e050746be7f
sarathsathish07 <sarathsathish1998@gmail.com>
2024-09-27 21:47:34 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000","https://celebrate-spaces.vercel.app","https://celebrate-spaces-kqbj.vercel.app"], 
+  origin: ["http://localhost:3000","https://gamitar-peach.vercel.app"], 
   credentials: true,  
 }));
 
