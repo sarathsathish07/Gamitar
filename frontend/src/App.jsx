ee84ba38f2d772c2f0966476bb37440da3ccaa62
sarathsathish07 <sarathsathish1998@gmail.com>
2024-11-11 19:29:20 +0530

    main container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <>
       <Header />
       <ToastContainer />
-      <Container className="my-2">
+      <Container className="my-2 main-container">
         <Outlet />
       </Container>
     </>
