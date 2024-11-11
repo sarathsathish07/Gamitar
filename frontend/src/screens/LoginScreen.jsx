9e423bb3325d9a78d3b2804b655333b3c4a8e14a
sarathsathish07 <sarathsathish1998@gmail.com>
2024-11-11 19:26:43 +0530

    styles

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -8,6 +8,7 @@ import { setCredentials } from "../slices/authSlice";
 import { toast } from "react-toastify";
 import Loader from "../components/Loader";
 import { io } from "socket.io-client";
+import "../styles/LoginScreen.css";  // Import the styles
 
 let socket;
 
@@ -44,42 +45,49 @@ const LoginScreen = () => {
   };
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="my-2" controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+    <div className="login-screen-container">
+      <FormContainer className="login-form-container">
+        <h1 className="login-header">Sign In</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="login-form-group" controlId="email">
+            <Form.Label className="login-form-label">Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="login-form-input"
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group className="my-2" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group className="login-form-group" controlId="password">
+            <Form.Label className="login-form-label">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="login-form-input"
+            ></Form.Control>
+          </Form.Group>
 
-        {isLoading && <Loader />}
+          {isLoading && <Loader />}
 
-        <Button type="submit" variant="primary" className="mt-3">
-          Sign In
-        </Button>
+          <Button type="submit" className="login-submit-button">
+            Sign In
+          </Button>
 
-        <Row className="py-3">
-          <Col>
-            New Customer? <Link to="/register">Register</Link>
-          </Col>
-        </Row>
-      </Form>
-    </FormContainer>
+          <Row className="py-3">
+            <Col>
+              New Customer?{" "}
+              <Link to="/register" className="login-link-text">
+                Register
+              </Link>
+            </Col>
+          </Row>
+        </Form>
+      </FormContainer>
+    </div>
   );
 };
 
