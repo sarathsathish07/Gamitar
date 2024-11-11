274269d7ac2422edd5077b21188768da2379421c
sarathsathish07 <sarathsathish1998@gmail.com>
2024-11-11 19:39:13 +0530

    uselocation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet,useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 
+  const location = useLocation();
   const isSignInPage = location.pathname === "/login";
   const isSignUpPage = location.pathname === "/register";
 
